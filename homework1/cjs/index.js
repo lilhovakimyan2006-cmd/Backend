586213b0b8a2e9ce9e4788592fcaab1e3170cc88
LILIT HOVAKIMYAN <lilithovakimyan@LILITs-MacBook-Air.local>
2026-08-11 19:27:09 +0400

    ESM and CJS understanding

---
+++ homework1/cjs/index.js
@@ -0,0 +1,9 @@
+const math = require("./utils/math");
+const capitalize = require("./utils/strings");
+
+console.log(math.add(5, 3));
+console.log(math.subtract(10, 6));
+console.log(math.multiply(5, 7));
+
+console.log(capitalize("hello"));
+console.log(capitalize("string"));
