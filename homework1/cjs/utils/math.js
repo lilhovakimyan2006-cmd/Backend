28830052a1f86733a3373657e0ddfc2b233b6c75
LILIT HOVAKIMYAN <lilithovakimyan@LILITs-MacBook-Air.local>
2026-08-17 00:50:14 +0400

    Remove duplicate homework1

--- homework1/cjs/utils/math.js
+++
@@ -1,9 +0,0 @@
-const add = (a, b) => a + b;
-
-const subtract = (a, b) => a - b;
-
-const multiply = (a, b) => a * b;
-
-exports.add = add;
-exports.subtract = subtract;
-exports.multiply = multiply;
